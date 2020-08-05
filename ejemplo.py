0c1427464893af63c1cd1cf6564cd1a7e9334075
Franco Liza <fliza@dc.uba.ar>
2020-08-05 00:52:44 -0300

    parser

--- ejemplo.py
+++ ejemplo.py
@@ -4,38 +4,49 @@ import sys
 
 # mi lista de tockens
 tokens = [
-	'INT',
-	'FLOAT',
-	'NOMBRE',
-	'SUMA',
-	'RESTA',
-	'DIVISION',
-	'MULTIPLICACION',
-	'IGUAL'
+	'CORCHETE_IZQ',
+	'CORCHETE_DER',
+	'PARENTESIS_IZQ',
+	'PARENTESIS_DER',
+	'LLAVES_IZQ',
+	'LLAVES_DER',
+	'COMILLAS',
+	'ESPACIO',
+	'RONDA',
+	'JUGADA',
+	'RESULTADO',
+	'CHAR'
 ]
 
-t_SUMA = r'\+'
-t_RESTA = r'\-'
-t_MULTIPLICACION = r'\*'
-t_DIVISION = r'\/'
-t_IGUAL = r'\='
+t_CORCHETE_IZQ = r'\['
+t_CORCHETE_DER = r'\]'
+t_PARENTESIS_IZQ = r'\('
+t_PARENTESIS_DER = r'\)'
+t_LLAVES_IZQ = r'\{'
+t_LLAVES_DER = r'\}'
+t_COMILLAS = r'\"'
+t_ESPACIO = r'\s'
 
-t_ignore = r' '
+#t_ignore = r' '
 
-## por ejemplo cadenas del tipo 1.1, 2.4, etc
-def t_FLOAT(t):
-	r'\d+\.\d+'
-	t.value = float(t.value)
+count = 0
+
+def t_JUGADA(t):
+	r'[PNBRQK]?[a-h]?[1-8]?x?[a-h][1-8](\+|\#)?|O-O|O-O-O'					#no sé muy bien a que se refiere
+	
 	return t
 
-def t_INT(t):
-	r'\d+'
-	t.value = int(t.value)
+def t_RONDA(t):
+	r'\d+(\.|\.\.\.)'
 	return t
 
-def t_NOMBRE(t):
-	r'[a-zA-Z_][a-zA-Z_0-9]*'
-	t.type = 'NOMBRE'
+def t_RESULTADO(t):
+	r'1-0|0-1|1/2-1/2'
+	return t
+
+def t_CHAR(t):
+	r'[\S\s]'
+	t.type = 'CHAR'
 	return t
 
 def t_error(t):
@@ -45,43 +56,107 @@ def t_error(t):
 
 lexer = lex.lex()
 
-def p_calculadora(p):
+def p_start(p):
 	'''
-	calculadora : expresion
-				| lamda 	
+	start 	: partida start 
+			| partida 	
 	'''
 
 	print(p[1])
 
 
-def p_expresion(p):
+def p_partida(p):
 	'''
-	expresion 	: expresion MULTIPLICACION expresion
-				| expresion DIVISION expresion
-				| expresion SUMA expresion
-				| expresion RESTA expresion
+	partida 	: metadata B
+
 	'''
 	p[0] = (p[2], p[1], p[3])
 
-def p_expresion_int_float(p):
+def p_metadata(p):
 	'''
-	expresion 	: INT
-				| FLOAT
-	'''
-	p[0] = p[1]
+	metadata 	: A1 metadata 
+				| A1
 
-#no hace nada
+	'''
+
+def p_A1(p):
+	'''
+	A1 		: CORCHETE_IZQ A2 ESPACIO COMILLAS A2 COMILLAS CORCHETE_DER
+
+	'''
+
+def p_A2(p):
+	'''
+	A2 		: CHAR A2
+			| CHAR
+
+	'''
+
+def p_B(p):
+	'''
+	B 		: B1 RESULTADO
+
+	'''
+
+def p_B1(p):
+	'''
+	B1 		: RONDA JUGADA comentario JUGADA comentario B1	
+			| RONDA JUGADA comentario  
+			| lamda
+
+	''' 		
+
+def p_comentario(p):
+	'''
+	comentario 	: PARENTESIS_IZQ B5 PARENTESIS_DER
+				| LLAVES_IZQ B5 LLAVES_DER
+				| lamda
+
+	'''
+
+def p_B5(p):
+	'''
+	B5 		: texto comentario JUGADA B5
+			| texto comentario B5
+			| lamda 
+
+	'''	
+
+def p_texto(p):
+	'''
+	texto 	: CHAR texto
+			| lamda
+
+	'''
+
 def p_lamda(p):
 	'''
 	lamda :
 	'''
-	p[0] = None
+	pass
+
+def p_error(p):
+     print("Syntax error in input!")
+
 
 parser = yacc.yacc()
 
+s = """[Event "Renova Group Grand Prix 2013"] 
+1. d4 d5 2. c4 c6 3. Nc3 1-0"""
+
+#while True:
+#	try:
+#	except EOFError:
+#		break
+
+lexer.input(s)
+ 
+ # Tokenize
 while True:
-	try:
-		s = input('')
-	except EOFError:
-		break
-	parser.parse(s)
+	tok = lexer.token()
+	if not tok:
+		break			# No more input
+	print(tok)
+
+
+#parser.parse(s)
